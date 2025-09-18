6febd8308128614fc2b2770e41911274e9f4685c
LAppStremio <lwebdesign22app@gmail.com>
2025-09-18 20:54:12 +0100

    Update playlist-transformer.js

--- playlist-transformer.js
+++ playlist-transformer.js
@@ -218,7 +218,7 @@ class PlaylistTransformer {
           poster: channel.tvg?.logo,
           background: channel.tvg?.logo,
           logo: channel.tvg?.logo,
-          description: `Canale: ${cleanName} - ID: ${finalChannelId}`,
+          description: `Canal: ${cleanName} - ID: ${finalChannelId}`,
           runtime: 'LIVE',
           behaviorHints: {
               defaultVideoId: `tv|${finalChannelId}`,
